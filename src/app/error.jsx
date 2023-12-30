4860a776ff237246c8f08158f9059a8554b4d125
Vaisakh <vaisakhpn78@gmail.com>
2023-12-30 22:54:36 +0530

    add loading effect

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -9,7 +9,7 @@ export default function Error({ error, reset }) {
   return (
     <div className="text-center mt-10 ">
       <h1>Something went wrong</h1>
-      <button className="hover:text-amber-600" onClick={() => reset()}>Try again</button>
+      <button className="hover:text-amber-600cd" onClick={() => reset()}>Try again</button>
     </div>
   );
 }
---
+++ src/app/loading.jsx
@@ -0,0 +1,8 @@
+"use client";
+export default function loading() {
+  return (
+    <div className="flex justify-center ">
+      <img className="h-95" src="spinner.svg" alt="Loading..." />
+    </div>
+  );
+}
