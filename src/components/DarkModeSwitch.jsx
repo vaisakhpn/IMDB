1d20b74ab5c2e46cd889683fdee60432142217da
Vaisakh <vaisakhpn78@gmail.com>
2023-12-30 11:28:07 +0530

    fetch data from tmdb database

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -6,8 +6,8 @@ import { useTheme } from "next-themes";
 export default function DarkModeSwitch() {
   const { systemTheme, theme, setTheme } = useTheme();
   const [mounted, setMounted ] = useState(false);
-
-  useEffect(() => setMounted(true), []);
+  useEffect(() =>
+   setMounted(true), []);
 
   const currentTheme = theme === "system" ? systemTheme : theme;
 
